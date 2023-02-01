a04b1437d901d0360d946fb70b54647d328baa25
William Schulte <jsfanatik@gmail.com>
2023-01-31 21:31:08 -0600

    styling and store updates
    - updated styling for navbar, login, and register pages
    - implemented functionality to handle active state for heart selectors
    - implemented functionality to prevent the saving of duplicate items

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,40 +1,101 @@
 <template>
-  <nav class="sticky top-0 bg-gray-300 border-gray-200 px-2 sm:px-4 py-1.5 z-40">
-    <div class="container flex flex-wrap items-center justify-between mx-auto">
-      <a href="https://flowbite.com/" class="flex items-center">
-        <img src="https://static.thenounproject.com/png/91211-200.png" class="h-10 mr-3 sm:h-9" alt="Flowbite Logo" />
-        <span class="self-center text-black text-xl font-semibold whitespace-nowrap">HipStore</span>
-      </a>
-      <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-cta">
-        <ul v-if="user" class="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 bg-gray-300 dark:border-gray-700">
-          <li v-for="item in navItems" :key="item.id" @click="getProducts(item)">
-            <a href="#" :class="[item.category === route.params.category ? 'text-blue-700' : 'text-gray-700']" class="block py-2 pl-3 pr-4 rounded md:bg-transparent md:p-0" aria-current="page">{{ item.name }}</a>
-          </li>
-        </ul>
-      </div>
-      <div class="flex md:order-2 hover:cursor-pointer">
-        <div v-if="user" class="flex">
-          <div @click="goToSaved" class="flex">
-              <HeartIcon class="w-7 h-7" :class="[route.name === 'Saved' ? 'text-blue-700' : '']"/>
-              <span class=" text-black text-xl">({{ savedItemsStore.savedItems.length || '0' }})</span>
-          </div>
-          <div @click="openModals" class="flex">
-              <ShoppingCartIcon class="w-7 h-7"/>
-              <span class=" text-black text-xl">({{ cartStore.cartItems.length || '0' }})</span>
-          </div>
-          <div class="flex">
-              <span @click="logout" class="ml-2 text-xl text-black text-md">Logout</span>
+  <Disclosure as="nav" class="sticky top-0 bg-gray-600 z-40" v-slot="{ open }">
+    <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+      <div class="flex h-14 items-center justify-between">
+        <div class="flex items-center">
+          <!-- <div class="flex-shrink-0">
+            <img class="h-8 w-8" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company" />
+          </div> -->
+          <div v-if="user" class="hidden md:block">
+            <div class="flex items-baseline space-x-4 hover: cursor-pointer">
+              <span v-for="item in navItems" :key="item.id" @click="getProducts(item)" :class="[item.category === route.params.category ? 'bg-gray-900 text-white' : 'text-gray-200 hover:bg-gray-700 hover:text-white', 'px-3 py-2 rounded-md text-md font-medium']" :aria-current="item.current ? 'page' : undefined">{{ item.name }}</span>
+            </div>
           </div>
         </div>
-        <!-- <div v-if="!user">
-          <div class="flex">
-            <router-link class="cursor-pointer text-black text-xl" :to="{name: 'Login'}">Login</router-link>
+        <div class="hidden md:block">
+          <div class="ml-4 flex items-center md:ml-6">
+            <button v-if="user" @click="goToSaved" type="button" class="p-1 text-gray-200 hover:text-white">
+              <span class="sr-only">View notifications</span>
+              <div class="flex">
+                <HeartIcon class="w-6 h-6" :class="[route.name === 'Saved' ? 'text-blue-400' : '']"/>
+                <span class="text-md">({{ savedItemsStore.savedItems.length || '0' }})</span>
+              </div>
+            </button>
+
+            <button v-if="user" @click="openModals" type="button" class="p-1 text-gray-200 hover:text-white">
+              <span class="sr-only">View notifications</span>
+              <div class="flex">
+                <ShoppingCartIcon class="w-6 h-6"/>
+                <span class="text-md">({{ cartStore.cartItems.length || '0' }})</span>
+              </div>
+            </button>
+
+            <button v-if="user" @click="logout" type="button" class="-mr-4 ml-2 p-1 text-gray-200 hover:text-white">
+              <span class="sr-only">View notifications</span>
+              <div class="flex">
+                <LogoutIcon class="w-6 h-6"/>
+              </div>
+            </button>
+
+            <!-- Profile dropdown -->
+            <Menu as="div" class="relative ml-3 z-50">
+              <div>
+                <MenuButton class="flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                  <span class="sr-only">Open user menu</span>
+                  <!-- <img class="h-8 w-8 rounded-full" :src="user.imageUrl" alt="" /> -->
+                </MenuButton>
+              </div>
+              <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
+                <MenuItems class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                  <MenuItem>
+                    <span class="block px-4 py-2 text-sm text-gray-700">Your Profile</span>
+                  </MenuItem>
+                  <MenuItem>
+                    <span class="block px-4 py-2 text-sm text-gray-700">Settings</span>
+                  </MenuItem>
+                  <MenuItem @click="logout" class="hover: cursor-pointer">
+                    <span class="block px-4 py-2 text-sm text-gray-700">Logout</span>
+                  </MenuItem>
+                </MenuItems>
+              </transition>
+            </Menu>
           </div>
-        </div> -->
+        </div>
+        <div class="-mr-2 flex md:hidden">
+          <!-- Mobile menu button -->
+          <DisclosureButton class="inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+            <span class="sr-only">Open main menu</span>
+            <MenuIcon v-if="!open" class="block h-6 w-6" aria-hidden="true" />
+            <XIcon v-else class="block h-6 w-6" aria-hidden="true" />
+          </DisclosureButton>
+        </div>
       </div>
-      
     </div>
-  </nav>
+
+    <DisclosurePanel class="md:hidden">
+      <div class="space-y-1 px-2 pt-2 pb-3 sm:px-3">
+        <DisclosureButton v-for="item in navItems" :key="item.id" @click="getProducts(item)" as="a" :href="item.href" :class="[item.category === route.params.category ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'block px-3 py-2 rounded-md text-base font-medium']" :aria-current="item.current ? 'page' : undefined">{{ item.name }}</DisclosureButton>
+      </div>
+      <div class="border-t border-gray-700 pt-4 pb-3">
+        <div class="flex items-center px-5">
+          <div class="flex-shrink-0">
+            <!-- <img class="h-10 w-10 rounded-full" :src="user.imageUrl" alt="" /> -->
+          </div>
+          <div class="ml-3">
+            <div class="text-base font-medium leading-none text-white">{{ user.name }}</div>
+            <div class="text-sm font-medium leading-none text-gray-400">{{ user.email }}</div>
+          </div>
+          <button @click="goToSaved" type="button" class="ml-auto flex-shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+            <span class="sr-only">View notifications</span>
+            <HeartIcon class="w-6 h-6" :class="[route.name === 'Saved' ? 'text-blue-400' : '']"/>
+          </button>
+        </div>
+        <div class="mt-3 space-y-1 px-2">
+          <DisclosureButton v-for="item in userNavigation" :key="item.name" as="a" :href="item.href" class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white">{{ item.name }}</DisclosureButton>
+        </div>
+      </div>
+    </DisclosurePanel>
+  </Disclosure>
     
   <Cart @closeModal="closeModal" :isOpen="isOpen"/>
 
@@ -53,9 +114,11 @@ import { useCartStore } from '../stores/cartStore';
 import { useUserStore } from '../stores/userStore';
 import { supabase } from '../supabase/init'
 import { useRoute, useRouter } from "vue-router";
-import { HeartIcon, ShoppingCartIcon } from "@heroicons/vue/outline";
+import { HeartIcon, ShoppingCartIcon, MenuIcon, XIcon, LogoutIcon } from "@heroicons/vue/outline";
 import Cart from '../components/Cart.vue'
 import CartEmpty from '../components/CartEmptyDialog.vue'
+
+import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
 
 const store = useAllProductsStore()
 const savedItemsStore = useSavedItemsStore()
@@ -66,6 +129,25 @@ const route = useRoute()
 const isOpen = ref(false)
 const isEmptyOpen = ref(false)
 const isEmptyMessage = ref("")
+
+// const user = {
+//   name: 'Tom Cook',
+//   email: 'tom@example.com',
+//   imageUrl:
+//     'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+// }
+const navigation = [
+  { name: 'Dashboard', href: '#', current: true },
+  { name: 'Team', href: '#', current: false },
+  { name: 'Projects', href: '#', current: false },
+  { name: 'Calendar', href: '#', current: false },
+  { name: 'Reports', href: '#', current: false },
+]
+const userNavigation = [
+  { name: 'Your Profile', href: '#', eventHandler: false },
+  { name: 'Settings', href: '#', eventHandler: false },
+  { name: 'Sign out', href: '#', eventHandler: true },
+]
 
 const navItems = [
   { name: "All", category: "all"},
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class="max-w-screen-sm mx-auto px-4 py-10">
-      <!-- Error handling-->
+    <!-- <div class="max-w-screen-sm mx-auto px-4 py-10">
+
       <div v-if="errorMsg" class="mb-10 p-4 rounded-md bg-light-grey shadow-lg">
         <p class="text-red-500">{{ errorMsg }}</p>
       </div>
-      <!--registration -->
+
       <form @submit.prevent="register" class="p-8 flex flex-col bg-light-grey rounded-md shadow-lg">
         <h1 class="text-3xl text-at-light-green mb-4">Register</h1>
         <div class="flex flex-col mb-2">
@@ -16,7 +16,6 @@
           id="email">
         </div>
   
-        <!-- password -->
         <div class="flex flex-col mb-2">
           <label for="password" class="mb-1 text-sm text-at-light-green">Password</label>
           <input
@@ -26,7 +25,6 @@
           id="password">
         </div>
   
-        <!-- confirm password -->
         <div class="flex flex-col mb-2">
           <label for="confirmPassword" class="mb-1 text-sm text-at-light-green">Confirm Password</label>
           <input
@@ -40,7 +38,52 @@
       
         <router-link class="text-sm mt-6 text-center" :to="{ name: 'Login' }">Already have an account? <span class="text-at-light-green">Login</span></router-link>
       </form>
+    </div> -->
+
+    <div class="flex min-h-full items-center justify-center py-36 px-4 sm:px-6 lg:px-8">
+    <div class="w-full max-w-md space-y-8 rounded-lg border-solid border-2 p-12 border-indigo-600">
+      <div>
+        <img class="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company" />
+        <h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">The Store</h2>
+        <p class="mt-2 text-center text-sm text-gray-600">
+          Or
+          {{ ' ' }}
+          <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">start your 14-day free trial</a>
+        </p>
+      </div>
+      <form @submit.prevent="register" class="mt-8 space-y-6" action="#" method="POST">
+        <input type="hidden" name="remember" value="true" />
+        <div class="space-y-6 rounded-md shadow-sm">
+          <div>
+            <label for="email-address" class="sr-only">Email address</label>
+            <input v-model="email" id="email-address" name="email" type="email" autocomplete="email" required="" class="relative block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Email address" />
+          </div>
+          <div>
+            <label for="password" class="sr-only">Password</label>
+            <input v-model="password" id="password" name="password" type="password" autocomplete="current-password" required="" class="relative block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Password" />
+          </div>
+        </div>
+
+        <div class="flex items-center justify-between">
+          <div class="flex items-center">
+            <input id="remember-me" name="remember-me" type="checkbox" class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500" />
+            <label for="remember-me" class="ml-2 block text-sm text-gray-900">Remember me</label>
+          </div>
+
+          <div class="text-sm">
+            <router-link class="font-medium text-indigo-600 hover:text-indigo-500" :to="{ name: 'Login' }">Already have an account?</router-link>
+            <!-- <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">Forgot your password?</a> -->
+          </div>
+        </div>
+
+        <div>
+          <button type="submit" class="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+            Register
+          </button>
+        </div>
+      </form>
     </div>
+  </div>
   </template>
   
   <script>
