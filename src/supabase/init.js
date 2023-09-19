8785958771f0bbf6c5d474f8b1a897b655de7f1a
VueStacks <49799116+jsfanatik@users.noreply.github.com>
2023-09-19 09:12:38 -0500

    Update init.js

--- src/supabase/init.js
+++ src/supabase/init.js
@@ -7,7 +7,7 @@
 
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://gyltaswaqxankvogihwn.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd5bHRhc3dhcXhhbmt2b2dpaHduIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzUzNzI3MTUsImV4cCI6MTk5MDk0ODcxNX0.IbmJ8o1XuXZij89_IwQ3VJkL7vYstlfZm6WnY7YK2Aw'
+const supabaseUrl = ''
+const supabaseAnonKey = ''
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
